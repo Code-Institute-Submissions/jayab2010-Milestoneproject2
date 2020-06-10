a2df6932a9e240207e420b61f96533f4f2168750
jaya bidwai <jayab2010@gmail.com>
2020-06-10 05:48:32 +0000

    added index.html,style.css

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// code for soft scolling of page
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
+
+
