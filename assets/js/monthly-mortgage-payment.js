ff14b8ca739c476a547e33135cb0e29d5cc00a53
jayab2010 <jayab2010@gmail.com>
2020-06-27 07:11:12 +0000

    remove error from monthly morgtgage calculator

--- assets/js/monthly-mortgage-payment.js
+++ assets/js/monthly-mortgage-payment.js
@@ -1,30 +1,59 @@
 
-			var results;
-			
-			function clearResults() {
-				$(".form__error").html("");
-				$('.results').html("");
-			};
-		
-			$(function(){
-				$('body').on('submit','form',{},function(e){
-					e.preventDefault();	
-					// clear previous results
-					clearResults();
-					// get our inputs
-					var rawInputs = $(this).serializeArray();
-					// convert these to numbers and create our argument array
-					var args = {};
-					for(var i = 0; i < rawInputs.length; i++) {
-						args[rawInputs[i].name] = parseFloat(rawInputs[i].value);
-					}
-					// run the mortgage calculator
-					results = mortgageCalculators.monthlyMortgagePayments(args);
-					if(results.error) {
-						$(".form__error").html(results.error);
-					} else {
-						$('.results').html('<p> Monthly mortgage payments of ' + results + '</p>')
-					}
-				});
-			});
-		
+(function($) {
+     $.fn.calculateMortgage = function(options) {
+    var defaults = {
+      currency: '&dollar;',
+      params: {}
+    };
+    options = $.extend(defaults, options);
+
+    var calculate = function(params) {
+      params = $.extend({
+        balance: 0,
+        rate: 0,
+        term: 0,
+        period: 0
+      }, params);
+
+      var N = params.term * params.period;
+      var I = (params.rate / 100) / params.period;
+      var v = Math.pow((1 + I), N);
+      var t = (I * v) / (v - 1);
+      var result = params.balance * t;
+
+      return result;
+
+    };
+
+    return this.each(function() {
+      var $element = $(this);
+      var $result = calculate(options.params);
+      var output = '<p>' + options.currency + ' ' + $result.toFixed(2) + '</p>';
+      $(output).appendTo($element);
+
+
+    });
+
+  };
+
+
+})(jQuery);
+
+$(function() {
+  $('#test').on('submit', function(e) {
+    e.preventDefault();
+    var $params = {
+      balance: $('#balance').val(),
+      rate: $('#rate').val(),
+      term: $('#term').val(),
+      period: $('option:selected', '#period').val()
+    };
+
+    $(this).calculateMortgage({
+      params: $params
+    })
+
+  });
+
+
+});
